2b29dccf6f26f94d5390178e9fa4e31adc391331
CRhACKER7 <crhacker7@gmail.com>
2024-12-03 13:38:39 +0100

    update groovy

--- app.groovy
+++ app.groovy
@@ -2,7 +2,7 @@ pipeline {
     agent any
 
     environment {
-        DOCKER_IMAGE = "albertprofehub/bookspageable"
+        DOCKER_IMAGE = "crhacker7hub/spring-boot-app"
         AWS_REGION = "us-east-1"
         ECR_REGISTRY = "<your-aws-account-id>.dkr.ecr.${AWS_REGION}.amazonaws.com"
         ECR_REPOSITORY = "bookspageable"
@@ -11,7 +11,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git 'https://github.com/AlbertProfe/BooksPageable.git'
+                git 'https://github.com/Crhacker7/BooksPageable.git'
             }
         }
 
